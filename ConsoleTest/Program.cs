41a492edad2682ee7c76685a20b263952a21b57b
St-Creamer <zakaria.hajlaoui@gmail.com>
2019-08-24 13:55:16 +0100

    search function in progress

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace ConsoleTest
@@ -10,7 +11,7 @@ namespace ConsoleTest
         {
             Console.WriteLine("1. add a car");
             Console.WriteLine("2. show all cars");
-
+            Console.WriteLine("3. search for a car");
             int answer = int.Parse(Console.ReadLine());
             
 
@@ -34,6 +35,30 @@ namespace ConsoleTest
                         //}
                         break;
 
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("type car id");
+                        int id = int.Parse(Console.ReadLine());
+                        string json = File.ReadAllText(@"c:\Users\RIP\Desktop\c# console\ConsoleTest\ConsoleTest\Cars.json");
+                        
+                        var cars = JsonConvert.DeserializeObject<List<Car>>(json);
+                        foreach(var car in cars)
+                        {
+                            if (car.Id == id)
+                            {
+                                Console.WriteLine("found car");
+                                Console.WriteLine(car.brand);
+                                Console.WriteLine(car.colour);
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("car not found");
+                            }
+                        }
+                        
+                        break;
                     }
             }
 
