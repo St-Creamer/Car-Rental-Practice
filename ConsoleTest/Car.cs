68a2d4bc263c27cc231bd9e6a1048d64d6ad112e
St-Creamer <zakaria.hajlaoui@gmail.com>
2019-08-24 01:24:48 +0100

    initial commit

---
+++ ConsoleTest/Car.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConsoleTest
+{
+    class Car
+    {
+        public int Id { get; set; }
+        public string brand { get; set; }
+        public string colour { get; set; }
+        public int milage { get; set; }
+
+        public  Car()
+        {
+            this.milage = 0;
+        }
+        public void AddCar(Car car)
+        {
+            Console.WriteLine("Enter Car Id");
+            this.Id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Car Brand");
+            this.brand = Console.ReadLine();
+
+            Console.WriteLine("Enter Car Colour");
+            this.colour = Console.ReadLine();
+
+            Console.WriteLine("Enter Car Milage Or \"n\" if not needed");
+
+            string x = Console.ReadLine();
+            if (x != "n")
+            {
+                this.milage = int.Parse(x);
+            }
+            using (StreamWriter file = File.AppendText(@"c:\Users\RIP\Desktop\c# console\ConsoleTest\ConsoleTest\Cars.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, this);
+            }
+        }
+
+
+    }
+}
---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. add a car");
+            Console.WriteLine("2. show all cars");
+
+            int answer = int.Parse(Console.ReadLine());
+            
+
+            
+            
+            switch (answer){
+               case 1 :
+                    {
+                        Car car = new Car();
+                        car.AddCar(car);
+                        break;
+                    }
+                case 2 :
+                    {
+                        string cars = File.ReadAllText(@"c:\Users\RIP\Desktop\c# console\ConsoleTest\ConsoleTest\Cars.json");
+                        Console.WriteLine(cars);
+                        //using (StreamReader file = File.OpenText(@"c:\Users\RIP\Desktop\c# console\ConsoleTest\ConsoleTest\Cars.json"))
+                        //{
+                        //    JsonSerializer serializer = new JsonSerializer();
+                        //    Car car = (Car)serializer.Deserialize(file, typeof(Car));
+                        //}
+                        break;
+
+                    }
+            }
+
+            
+
+
+        }
+    }
+}
