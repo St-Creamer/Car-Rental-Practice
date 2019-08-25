eb64d244d975664de3ebaaecd3e3d53c85d2ee1a
St-Creamer <zakaria.hajlaoui@gmail.com>
2019-08-25 15:45:32 +0100

    menu fixed and some minor bugs

--- ConsoleTest/CarHelper.cs
+++ ConsoleTest/CarHelper.cs
@@ -11,31 +11,35 @@ namespace ConsoleTest
     {
         public static List<Car> RetrieveCars()
         {
-            var cars = new List<Car>();
+            
             string json = File.ReadAllText(@"c:\Users\RIP\Desktop\c# console\ConsoleTest\ConsoleTest\Cars.json");
-            var retrievedlist = JsonConvert.DeserializeObject<List<Car>>(json);
-            return retrievedlist;
-        }
-        public static void SendListToFile(List<Car> cars)
-        {
-            if (File.Exists(@"c:\Users\RIP\Desktop\c# console\ConsoleTest\ConsoleTest\Cars.json"))
+            if(json == "")
             {
-                
-                
-                string json = JsonConvert.SerializeObject(cars);
-                using (StreamWriter sw = File.AppendText(@"c:\Users\RIP\Desktop\c# console\ConsoleTest\ConsoleTest\Cars.json"))
-                {
-                    sw.WriteLine(json);
-                }
+                var cars = new List<Car>();
+                return cars;
             }
             else
             {
-                string json = JsonConvert.SerializeObject(cars);
-                using (StreamWriter sw = File.CreateText(@"c:\Users\RIP\Desktop\c# console\ConsoleTest\ConsoleTest\Cars.json"))
-                {
-                    sw.WriteLine(json);
-                }
+                var retrievedlist = JsonConvert.DeserializeObject<List<Car>>(json);
+                return retrievedlist;
             }
+            
+        }
+        public static void SendListToFile(List<Car> cars)
+        {
+            if (!File.Exists(@"c:\Users\RIP\Desktop\c# console\ConsoleTest\ConsoleTest\Cars.json"))
+            {
+
+
+                File.Create(@"c:\Users\RIP\Desktop\c# console\ConsoleTest\ConsoleTest\Cars.json");
+            }
+           
+            string json = JsonConvert.SerializeObject(cars);
+            using (StreamWriter sw = File.CreateText(@"c:\Users\RIP\Desktop\c# console\ConsoleTest\ConsoleTest\Cars.json"))
+             {
+                sw.WriteLine(json);
+             }
+            
         }
 
         public static void AddCar(Car car)
--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -9,85 +9,100 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1. add a car");
-            Console.WriteLine("2. show all cars");
-            Console.WriteLine("3. search for a car");
-            Console.WriteLine("4. Edit an existing car");
-            int answer = int.Parse(Console.ReadLine());
+            bool run = true;
 
+            while (run)
+            {
 
-            Car car = new Car();
-            var cars = new List<Car>();
+                Console.WriteLine("###################################");
+                Console.WriteLine("1. add a car");
+                Console.WriteLine("2. show all cars");
+                Console.WriteLine("3. search for a car");
+                Console.WriteLine("4. Delete a car");
+                Console.WriteLine("6. to quit");
+                Console.WriteLine("###################################");
+                int answer = int.Parse(Console.ReadLine());
+                Console.WriteLine("\n");
 
-            switch (answer){
-               case 1 :
+                Car car = new Car();
+                var cars = new List<Car>();
+
+                switch (answer){
+                    case 1 :
                     {
                         
                         CarHelper.AddCar(car);
-                        break;
+                            Console.WriteLine("\n");
+                            break;
                     }
-                case 2 :
+                    case 2 :
                     {
                          cars = CarHelper.RetrieveCars();
                         foreach(var item in cars)
                         {
-                            Console.WriteLine("Car Id: " + item.Id + "\n Car Brand: " + item.brand + "\n Car Milage :" + item.milage.ToString());
+                            Console.WriteLine("Car Id: " + item.Id + "\n Car Brand: " + item.brand + "\n Car Milage: " + item.milage.ToString());
                         }
-                        break;
+                            Console.WriteLine("\n");
+                            break;
 
                     }
-                case 3:
+                    case 3:
                     {
                         Console.WriteLine("type car id");
                         int id = int.Parse(Console.ReadLine());
                          cars = CarHelper.RetrieveCars();
+                        bool x = false;
                         foreach(var item in cars)
                         {
                             if (item.Id == id)
                             {
+                                x = true;
                                 Console.WriteLine("found car");
                                 Console.WriteLine("car brand : "+item.brand);
                                 Console.WriteLine("car colour :"+item.colour);
                                 Console.WriteLine("car milage :"+item.milage.ToString());
 
                             }
-                            else
-                            {
-                                Console.WriteLine("car not found");
-                            }
+                           
                         }
-                        
-                        break;
+                        if (!x)
+                        {
+                            Console.WriteLine("Car not found");
+                        }
+                            Console.WriteLine("\n");
+                            break;
                     }
                 case 4:
                     {
 
                         Console.WriteLine("type car id");
                         int id = int.Parse(Console.ReadLine());
-
+                            int index = -1;
                          cars = CarHelper.RetrieveCars();
                         foreach (var item in cars)
                         {
                             if (item.Id == id)
-                            {
-                                
-                                cars.Remove(item);
-                                Console.WriteLine("Car with id" + id + " has been removed");
-                                CarHelper.SendListToFile(cars);
-                                
-
-                            }
+                            {index = cars.IndexOf(item);}
                             else
+                            {Console.WriteLine("car not found");}
+                            if(index != -1)
                             {
-                                Console.WriteLine("car not found");
+                                 cars.Remove(item);
+                                 Console.WriteLine("Car with id:" + id + " has been removed");
+
                             }
+                             CarHelper.SendListToFile(cars);
                         }
-
-                        break;
+                       
+                            Console.WriteLine("\n");
+                            break;
                     }
-            }
 
-            
+                    case 6: { run = false; break; }
+                        
+                }
+
+            }
 
 
         }
